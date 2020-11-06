73bcc6abb8f168a2c0d4fe94e4c80bc7b88eda9f
liuting <angliuting@gmail.com>
2020-11-06 12:12:42 +0800

    query from database completed

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ const BASE_URL = 'https://api.nytimes.com/svc/books/v3/reviews.json'
 
 // SQL 
 const SQL_FIND_BY_FIRSTCHAR = 'select title from book2018 where title like ? order by title asc limit ? offset ?'
+const SQL_COUNT = 'select count(*) as q_count from book2018 where title like ?'
+const SQL_FIND_BY_TITLE = 'select * from book2018 where title=?'
 
 // create the database connection pool
 const pool = mysql.createPool({
@@ -52,6 +54,29 @@ const startApp = async (app, pool) => {
     }
 }
 
+const mkQuery = (sqlStmt, pool) => {
+    const f = async (params) => {
+        const conn = await pool.getConnection()
+        try {
+            const results = await conn.query(sqlStmt, params)
+            console.info('results from mkquery', results[0])
+            return results[0]
+            }
+        catch(e) {
+            return Promise.reject(e)
+        }
+        finally {
+            conn.release()
+        }
+    }
+    return f
+}
+
+//Making queries
+const getBooksList = mkQuery(SQL_FIND_BY_FIRSTCHAR, pool)
+const getCount = mkQuery(SQL_COUNT, pool)
+const getBook = mkQuery(SQL_FIND_BY_TITLE, pool)
+
 // create an instance of application
 const app = express()
 
@@ -60,14 +85,13 @@ app.engine('hbs', handlebars({ defaultLayout: 'default.hbs' }))
 app.set('view engine', 'hbs')
 
 //morgan logging
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 
 // configure the application
 app.get('/', (req, resp) => {
     const alphabets = 'ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789'
     const search = alphabets.split('')
-    console.info('search', search)
     resp.status(200)
     resp.type('text/html')
     resp.render('index',
@@ -76,50 +100,76 @@ app.get('/', (req, resp) => {
     })
 })
 
-/*app.get('/search',
-    async (req, resp) => {
-        try {
-            //value of button
-            const search = req.query['search']
-            const author = req.query['author']
-
-            console.info(`search term is: ${search}`)
-
-            //construct the url
-            let url = withQuery(BASE_URL, {
-                [api-key]: API_KEY,
-                title: search,
-                author
-            })
-
-            let result = await fetch(url)
-            let reviews = await result.json()
-            const names = reviews.results
-            .map(
-                d => {
-                    return {title: d.book_title, author: d.book_author, reviewer: d.byline, summary: d.summary}
-                }
-            )
-            console.info('names: ', names, 'count: ', count)
-            resp.status(200)
+//searching and displaying titles
+app.get('/search', async(req, resp) => { 
+    try {
+        const limit = 10
+        const search = req.query['search']
+        const offset = parseInt(req.query['offset']) || 0
+        
+        let count = await getCount([ `${search}%` ])
+        const queryCount = count[0].q_count
+        const currentPage = offset/limit || 0
+        const pages = Math.floor(queryCount/limit)
+        const recs = await getBooksList([`${search}%`, limit, offset])
+        
+        if (recs.length <= 0) {
+            //404!
+            resp.status(404)
             resp.type('text/html')
-            resp.render('result',
-                {
-                    search,
-                    names, //key and value same name
-                    count,
-                    hasContent: count > 0
-                }
-            )
-        }
-        catch(e) {
-            resp.status(500)
-            resp.json(e)
-            console.error('Error: ', e)
+            resp.send(`Not found: ${search}`)
+            return
         }
 
+        resp.status(200)
+        resp.type('text/html')
+        resp.render('displayTitle', {
+            titles: recs,
+            search,
+            page: currentPage,
+            onePage: currentPage == 0 && currentPage == pages,
+            noNextPage: currentPage == pages,
+            noPrevPage: currentPage == 0,
+            prevOffset: Math.max(0, offset - limit),
+            nextOffset: offset + limit
+        })
     }
-)
-*/
+    catch(e) {
+        resp.status(500)
+        resp.type('text/html')
+        resp.send(JSON.stringify(e))
+    }
+})
+app.get('/search/:search', async(req, resp) => { 
+    try {
+        const search = req.params['search']
+        console.info('search', search)
+        const recs = await getBook([`${search}`])
+        const authors = recs[0].authors.split('|')
+        
+        if (recs.length <= 0) {
+            //404!
+            resp.status(404)
+            resp.type('text/html')
+            resp.send(`Not found: ${search}`)
+            return
+        }
+        
+        console.info('recs',recs)
+        console.info('authors', authors)
+        resp.status(200)
+        resp.type('text/html')
+        resp.render('bookDetail', {
+            book: recs[0],
+            authors
+        })
+    }
+    catch(e) {
+        resp.status(500)
+        resp.type('text/html')
+        resp.send(JSON.stringify(e))
+    }
+})
+
 //run function to start server
 startApp(app, pool)
